5c6830ff6b55e034e2ccefb8da5729ce5507da92
Nima Hejazi <nima@robotkudos.com>
2020-08-23 13:09:02 -0700

    Fix unrequired file rename; better log output

--- folder-merger.js
+++ folder-merger.js
@@ -1,34 +1,49 @@
 #!/usr/local/bin/node
 const fs = require('fs');
 const path = require('path');
+const chalk = require('chalk');
 
 const outputFolder = 'Output';
+let totalFiles = 0;
 
 if (!fs.existsSync('Output')) {
     fs.mkdirSync('Output');
 }
 
-
-fs.readdirSync('.').forEach(file => {
-    moveFiles(file, outputFolder);
-});
-
-function moveFiles(srcFolder, destFolder) {
-    let stats;
-    fs.readdirSync(srcFolder).forEach(file => {
-        if (file !== '.DS_Store') {
-            stats = fs.statSync(path.join(srcFolder, file));
-            if (stats.isFile()) {
-                const availableFilename = getFilename(path.basename(file));
-                fs.renameSync(path.join(srcFolder,file), path.join(destFolder, availableFilename));
-            } else {
-                moveFiles(file, destFolder);
-            }
-
-        }
-    });
-
+function success(msg) {
+    console.log(chalk.bgGreen('Success:'), msg);
 }
+
+function error(msg, exitCode = 1) {
+    console.log(chalk.white.bgRed('Error:'), msg);
+    process.exit(exitCode);
+}
+
+moveFiles('.', outputFolder);
+
+function moveFiles(src, destFolder) {
+    let stats = fs.statSync(src);
+    if (stats.isDirectory()) {
+        // make sure we don't go through outputFolder which we are moving files to
+        if (src == destFolder) return;
+        fs.readdirSync(src).forEach(file => {
+            moveFiles(path.join(src, file), outputFolder);
+        });
+    } else {
+        const availableFilename = getFilename(path.basename(src));
+        try {
+            const dest = path.join(destFolder, availableFilename);
+            fs.renameSync(src, dest);
+            totalFiles++;
+            success(src + chalk.yellow(' => ') + dest);
+        } catch (err) {
+            error(`Error occured: ${err}`);
+        }
+    }
+}
+
+console.log('------------------------');
+console.log('Total files movied: ' + chalk.green(totalFiles));
 
 // check if the filename is already exits in output 
 // and return a new one with a number index
@@ -41,5 +56,4 @@ function getFilename(filename) {
         file = path.basename(file, ext) + ' (' + ++i + ')' + ext;
     }
     return file;
-
 }
